b7c9c5f14562dd6d2d445a572d010d75f432a826
Dario Jularic <dariojularic0@gmail.com>
2024-12-27 22:08:25 +0100

    Refactor code

--- src/calculatorSlice.js
+++ src/calculatorSlice.js
@@ -26,13 +26,13 @@ const calculatorSlice = createSlice({
         state.operation = action.payload
         state.firstNumber = ""
       }
-      // if (state.secondNumber !== "") state.operation = action.payload
-      // if (state.firstNumber !== "" && state.operation !== "" && state.secondNumber !== "") {
-      //   console.log("state.operation", state.operation)
-      //   console.log("state.firstNumber", state.firstNumber)
-      //   console.log("state.secondNumber", state.secondNumber)
-      //   if (action.payload === "=") {
-      //     state.firstNumber = {}
+      if (state.secondNumber !== "") state.operation = action.payload
+      if (state.firstNumber !== "" && state.operation !== "" && state.secondNumber !== "") {
+        console.log("state.operation", state.operation)
+        console.log("state.firstNumber", state.firstNumber)
+        console.log("state.secondNumber", state.secondNumber)
+        if (action.payload === "=") {
+          state.firstNumber = {}
         }
         if (state.operation === "+") state.secondNumber = parseInt(state.secondNumber) + parseInt(state.firstNumber)
         if (state.operation === "-") state.secondNumber = state.secondNumber - state.firstNumber
