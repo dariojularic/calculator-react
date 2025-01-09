d1c3d8903df0c28149e9f8f75d17278a62a43988
Dario Jularic <dariojularic0@gmail.com>
2025-01-09 16:51:23 +0100

    Refactor code

--- src/components/CalculatorActionButton.jsx
+++ src/components/CalculatorActionButton.jsx
@@ -9,13 +9,3 @@ const CalculatorActionButton = ({ value, classValue, handler }) => {
 };
 
 export default CalculatorActionButton;
-
-// da odem sa route stranice ("/") na stranicu "menu" trebam pogledat react - router
-// trebat ce mi redux da spremim usera
-// pogledati react router komponentu <SharedLayout /> u dokumentaciji i youtube
-
-// https://react-fast-pizza-api.onrender.com/api/menu
-
-// napravit pocetnu stranu:
-// kad ukucam nesto u input, pojavi se dugme start ordering
-// kad submitam user se spremi u state i navigujem na "Menu" stranicu samo h1 i onda se cujemo
